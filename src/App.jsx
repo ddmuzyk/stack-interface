89238e6a07cb1a2527f0cfdca45ccfd7ee850fa2
ddmuzyk <domino2k1@gmail.com>
2024-03-31 18:30:08 +0200

    added the table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import { CircularProgress, Pagination } from '@mui/material'
+import { CircularProgress, Pagination, TablePagination } from '@mui/material'
+import BasicTable from './components/Table/Table'
 
 function App() {
   const [page, setPage] = useState(1)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }
+  , [page])
 
   return (
     <main>
-      <CircularProgress
+      {isLoading ? <CircularProgress
         sx={{
           width: '100px !important',
           height: '100px !important',
+          marginBottom: '50px',
+          marginTop: '50px',
         }}
-      />
+      /> : 
+      <BasicTable/>
+      }
       <Pagination sx={{
         '& .MuiPaginationItem-root': {
-          marginTop: '60px',
+          marginBottom: 'auto',
         }
         
       }} count={10000} color="primary" onChange={(e, value) => {
